916979cc12e52d20a05e85394593065783ff7818
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-09-23 21:08:41 +0530

    Update Utilities.groovy

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -8,7 +8,10 @@ class Utilities implements Serializable {
     //M3 is configured to MAVEN_HOME in Jenkins > Manage Jenkins > Global Tool Configuration > Maven 
     //> Click on "Add Maven", then give name as "M3" and MAVEN_HOME as "path/to/your/maven"
     steps.sh "export JAVA_HOME=${steps.tool 'Java8'}"
+    steps.sh "echo 'printing java home'"
     steps.sh "echo $JAVA_HOME"
+    steps.sh "echo 'printed java home'"
+    
     //steps.sh "export PATH=${steps.tool 'Java8'}/bin"
     steps.sh "${steps.tool 'M3'}/bin/mvn -o ${args}"
   }
