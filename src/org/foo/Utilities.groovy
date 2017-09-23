75e1a94a8d3a731a4734d0dac6a2b2795d04c429
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-09-23 20:27:36 +0530

    Update Utilities.groovy

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -1,9 +1,11 @@
 package org.foo
 class Utilities implements Serializable {
   def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
+  Utilities(steps) {
+    this.steps = steps
     println "steps: "+ steps
+  }
+  def mvn(args) {
     //M3 is configured to MAVEN_HOME in Jenkins > Manage Jenkins > Global Tool Configuration > Maven 
     //> Click on "Add Maven", then give name as "M3" and MAVEN_HOME as "path/to/your/maven"
     steps.sh "${steps.tool 'M3'}/bin/mvn -o ${args}"
