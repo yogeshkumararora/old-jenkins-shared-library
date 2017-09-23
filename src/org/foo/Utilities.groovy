35f37bb0a6487a226a3d4f204755bc740e664639
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-09-24 00:32:01 +0530

    Update Utilities.groovy

--- src/org/foo/Utilities.groovy
+++ src/org/foo/Utilities.groovy
@@ -6,17 +6,12 @@ class Utilities implements Serializable {
   }
   def checkOutFrom(repo) {
     steps.sh "rm -rf experiments"
-    //steps.sh "git status"
     steps.sh "git clone 'https://github.com/yogeshkumararora/${repo}'"
-    
-    
-    
+   
   }
   def mvn(args) {
     //M3 is configured to MAVEN_HOME in Jenkins > Manage Jenkins > Global Tool Configuration > Maven 
     //> Click on "Add Maven", then give name as "M3" and MAVEN_HOME as "path/to/your/maven"
-    //steps.sh "pwd"
     steps.sh "cd experiments;export JAVA_HOME=${steps.tool 'Java8'};${steps.tool 'M3'}/bin/mvn -o ${args}"
-    //steps.sh "rm -rf experiments"
   }
 }
