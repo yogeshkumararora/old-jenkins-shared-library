5b0d1f680ebf2c3e4f573729851753aab9b5891b
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-09-23 15:25:05 +0530

    Delete Utilities.groovy

--- resources/org/foo/Utilities.groovy
+++
@@ -1,8 +0,0 @@
-package org.foo
-class Utilities implements Serializable {
-  def steps
-  Utilities(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-}
