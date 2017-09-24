477ebce6c5c1101503b94817864854dae7857fc9
Yogesh Kumar Arora <yogeshkumararora@users.noreply.github.com>
2017-09-24 16:43:57 +0530

    Create acme.groovy

---
+++ vars/acme.groovy
@@ -0,0 +1,12 @@
+class acme implements Serializable {
+    private String name
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
